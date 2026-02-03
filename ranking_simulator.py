4aff3861e197e4a2e82430bfb66522fd85efccf3
bhaviksuthar-zepto <bhavik.suthar@zeptonow.com>
2026-02-03 20:08:28 +0530

    Update ranking_simulator.py

--- ranking_simulator.py
+++ ranking_simulator.py
@@ -4,6 +4,7 @@ import numpy as np
 
 st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
 
+df = pd.read_csv("base_file.csv")
 all_users = ["Alice", "Bob", "Charly"]
 with st.container(border=True):
     users = st.multiselect("Users", all_users, default=all_users)
@@ -15,5 +16,5 @@ if rolling_average:
     data = data.rolling(7).mean().dropna()
 
 tab1, tab2 = st.tabs(["Chart", "Dataframe"])
-tab1.line_chart(data, height=250)
-tab2.dataframe(data, height=250, use_container_width=True)
+tab1.line_chart(df, height=250)
+tab2.dataframe(df, height=250, use_container_width=True)
