875ac0737eb0c51aff8dc77fa3d92119611b234f
bhaviksuthar-zepto <bhavik.suthar@zeptonow.com>
2026-02-04 09:55:00 +0530

    Update ranking_simulator.py

--- ranking_simulator.py
+++ ranking_simulator.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
+st.write("Ranking Simulator")
 @st.cache_data
 def load_data():
     return pd.read_csv("base_file.csv")
