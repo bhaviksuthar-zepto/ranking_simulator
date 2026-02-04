5b5aa5117e34c134ae58e4e0437324716266fa89
bhaviksuthar-zepto <bhavik.suthar@zeptonow.com>
2026-02-04 10:08:51 +0530

    Update ranking_simulator.py

--- ranking_simulator.py
+++ ranking_simulator.py
@@ -13,7 +13,7 @@ st.sidebar.title("Ranking Simulator")
 # ---- Controls ----
 query = st.sidebar.selectbox(
     "Search Term",
-    options=search_terms
+    options=df["search_term"]
 )
 
 w_asp = st.sidebar.slider("ASP Boost Weight", -2.0, 2.0, 1.0, 0.1)
